04e682ab503f7f0ee89e293e1e86528dbe0e4c36
Dennis <denjand91@gmail.com>
2023-02-07 14:06:57 -0500

    fixed variable

--- config.js
+++ config.js
@@ -4,6 +4,8 @@ const PORT = +process.env.PORT || 3001;
 
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
+const OPEN_WEATHER_API_KEY = proccess.env.OPEN_WEATHER_API_KEY
+
 function getDatabaseUri() {
   return process.env.NODE_ENV === "test"
     ? "postgresql://dennis:nopassword@localhost/listmaker_test"
@@ -13,6 +15,7 @@ function getDatabaseUri() {
 
 
 module.exports = {
+  OPEN_WEATHER_API_KEY,
   getDatabaseUri,
   SECRET_KEY,
   PORT,
